c1a21807b08ea477d699e199607d30ae892c4658
lyautey3u <lyautey3u@ensem-305vpc14.ad.univ-lorraine.fr>
2019-11-25 11:59:51 +0100

    Sprint 1

---
+++ src/Demo_du_jeu/Test.java
@@ -0,0 +1,11 @@
+package Demo_du_jeu;
+
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		Labyrinthe lab = new Labyrinthe("Labyrinthe.txt");
+	}
+
+}
