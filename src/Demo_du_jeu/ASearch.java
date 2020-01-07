a88fca9ed1a985a6d18f5307e2da2c1fdc2d024d
Jin Yujun <jyjmarc@gmail.com>
2020-01-07 19:43:37 +0100

    changer les photos de Fantone

---
+++ src/Demo_du_jeu/ASearch.java
@@ -0,0 +1,12 @@
+package Demo_du_jeu;
+
+public class ASearch {
+	static class Node{
+		int f,g,h;
+		Node(int f,int g,int h){  
+            this.f=f;
+            this.g=g;
+            this.h=h;
+		}
+	}
+}
